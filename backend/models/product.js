60d4ecc85aad8fe072d9edb54d572a1aaea25e83
Sazzad <netsazzad@gmail.com>
2021-09-02 23:47:03 +0600

    product models created

---
+++ backend/models/product.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+    },
+
+    title: {
+      type: String,
+    },
+
+    description: {
+      type: String,
+    },
+
+    link: {
+      type: String,
+    },
+    image_link: {
+      type: String,
+    },
+    availability: {
+      type: String,
+    },
+    price: {
+      type: String,
+    },
+    brand: {
+      type: String,
+    },
+    gtin: {
+      type: String,
+    },
+    mpn: {
+      type: String,
+    },
+    condition: {
+      type: String,
+    },
+    shipping: {
+      type: String,
+    },
+    identifier_exists: {
+      type: String,
+    },
+    google_product_category: {
+      type: String,
+    },
+    adult: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+
+    color: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", ProductSchema);
+module.exports = Product;
