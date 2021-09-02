94332b9fa0f87a2500c40d4b39a07653510e9027
Sazzad <netsazzad@gmail.com>
2021-09-03 02:54:25 +0600

    server.js file added

--- backend/middlewares/index.js
+++ backend/middlewares/index.js
@@ -1,9 +1,15 @@
+"use strict";
+
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+
+const { notFound, errorHandler } = require("./errorHandler");
 
 exports.loadMiddlewares = function (app) {
   app.use(express.json());
   app.use(cors());
   app.use(helmet());
+  app.use(notFound);
+  app.use(errorHandler);
 };
---
+++ backend/server.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const express = require("express");
+const middleware = require("./middlewares");
+const router = require("./routes");
+const connect = require("./src/config/db");
+
+const app = express();
+
+middleware.loadMiddlewares(app); //loading all the middlewares
+router.loadRoutes(app); //Router configuration
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+(async () => {
+  await connect();
+
+  const PORT = process.env.PORT | 5000;
+  app.listen(PORT, () => {
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    );
+  });
+})();
