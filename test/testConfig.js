9918789e79ea66061233a5bb9933797816b6f7b0
Sazzad <netsazzad@gmail.com>
2021-09-05 17:16:49 +0600

    testConfig.js file updated

--- test/testConfig.js
+++ test/testConfig.js
@@ -1,7 +1,5 @@
 //During the automated test the env variable, We will set it to "test"
 process.env.NODE_ENV = "test";
-process.env.MONGO_URL =
-  "mongodb+srv://mern-ecom123:mern-ecom123@mern-ecommerce.ksbpm.mongodb.net/shoparize?retryWrites=true&w=majority";
 
 //Require the dev-dependencies
 let chai = require("chai");
